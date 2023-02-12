15d652a6bfe7d52f8404161e60e23c2ee4f49831
AnOrdinaryPeople <muhamad.odd@gmail.com>
2023-02-13 01:54:04 +0700

    🎨 Improve eslint and editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,37 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   rules: {
     indent: ['error', 2],
     quotes: ['error', 'single'],
-    semi: ['error', 'never']
-  }
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'ignore',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 }
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { fileURLToPath } from 'url'
+import { sync } from 'glob'
+import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite'
+
+function dirs(path) {
+  return sync(path).map((file) => fileURLToPath(new URL(file, import.meta.url)))
+}
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.ts',
-                'resources/js/user/index.ts',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: [
+        ...dirs('resources/js/**/*.ts'),
+        ...dirs('resources/sass/**/*.scss'),
+      ],
+      refresh: true,
+    }),
+  ],
+})
